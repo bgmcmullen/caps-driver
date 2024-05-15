810312a03fc196d581d3b17c3957d2e911e0134c
Brendan <bgmcmullen@comcast.net>
2024-05-15 18:55:51 -0400

    added queues

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ setInterval(driverReady, 5000);
 
 function handlePackageReadyForPickup(payload){
   console.log('----------------------');
-  console.log(`DRIVER SAYS: I picked up order ${payload.orderId}`)
+  console.log(`DRIVER SAYS: I picked up order ${payload.orderId} from ${payload.store}`)
   hubConnection.emit('in-transit', payload);
-  console.log(`DRIVER SAYS: I delivered order ${payload.orderId}`)
+
+  console.log(`DRIVER SAYS: I delivered order ${payload.orderId} to ${payload.customer}`)
   hubConnection.emit('delivered', payload);
 }
