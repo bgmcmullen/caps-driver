bc36cf995f5d5ddcb949c623bbb9073ecc71e6d5
Brendan <bgmcmullen@comcast.net>
2024-05-15 16:04:10 -0400

    queue added

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const URL = process.env.URL;
 
 const hubConnection = io.connect(URL);
 
-const handlePackageReadyForPickup = require('./handler.js');
-
 hubConnection.on('package-ready-for-pickup', handlePackageReadyForPickup);
+
+function driverReady(){
+  hubConnection.emit('driver-ready')
+}
+
+setInterval(driverReady, 5000);
+
+function handlePackageReadyForPickup(payload){
+  console.log('----------------------');
+  console.log(`DRIVER SAYS: I picked up order ${payload.orderId}`)
+  hubConnection.emit('in-transit', payload);
+  console.log(`DRIVER SAYS: I delivered order ${payload.orderId}`)
+  hubConnection.emit('delivered', payload);
+}
